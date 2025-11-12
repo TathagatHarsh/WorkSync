d8215e535542737e16f2986bc2c4af2146223be6
Tathagat Harsh <73013331+TathagatHarsh@users.noreply.github.com>
2025-11-12 16:46:10 +0530

    link backend

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000", // backend server
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    (import.meta.env.DEV
+      ? "http://localhost:3000"
+      : "https://worksync-xj7r.onrender.com"),
 });
 
 export default api;
